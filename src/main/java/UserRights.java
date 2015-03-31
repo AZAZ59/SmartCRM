4bc1e6f1eb48e040e715c545a7b59e74e2c111e3
YOUR NAME <Paradox11>
2015-03-31 12:25:21 +0400

    add new entities

--- src/main/java/UserRights.java
+++ src/main/java/UserRights.java
@@ -1,6 +1,16 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * Created by Grand on 31.03.2015.
  */
+@Entity
 public class UserRights {
+    @Id
+    private Integer id;
+    @Column
+    Integer rights; //0 - обычный юзер, 1 - админ
+    
 
 }
